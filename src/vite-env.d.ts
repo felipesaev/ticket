aa3f2b2f55b6576b7504e52f01200ab2365d5273
Felipe Costa <felipesaev@gmail.com>
2023-07-12 08:24:42 -0300

    feat: create login/signup

---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+BROWSER=none
+
+VITE_SUPABASE_URL='https://oyvlmrxflndhbcpzlubn.supabase.co'
+
+VITE_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95dmxtcnhmbG5kaGJjcHpsdWJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg0Njk2ODMsImV4cCI6MjAwNDA0NTY4M30.-xsQ67tIBGGRvY8Q_79mYapFYLRipunslS6Kgc_YvgI'
