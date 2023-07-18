39e9463f69501e38aef97c108ac54520bdbe5f48
Felipe Costa <felipesaev@gmail.com>
2023-07-18 18:40:59 -0300

    feat: create eslint and format with prerrier

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
-import AppRouter from './routes.tsx';
-
+import AppRouter from './routes.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <div className="container mx-auto">
-      <AppRouter />
+    <AppRouter />
   </div>
 )
